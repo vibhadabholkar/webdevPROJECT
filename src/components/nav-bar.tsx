bb67e61a873ed6b31e28e88537722ea66d3bb706
vibhadabholkar <90001502+vibhadabholkar@users.noreply.github.com>
2022-09-07 22:32:11 -0700

    Update nav-bar.tsx

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,7 +1,13 @@
 import { Box, Image, Heading, Text, HStack, Spacer } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from 'next/router'
+import Landing from "../pages/landing";
+import Voting from "../pages/voting";
 
 const NavBar = () => {
+
+  const router = useRouter()
+
   return (
     <Box px={12} py={2} bg="black">
       <HStack align="center" spacing={4}>
@@ -11,11 +17,13 @@ const NavBar = () => {
         </Heading>
         <Spacer />
         <HStack color="white" fontSize="2xs" fontWeight="semibold">
-          <Text>HOME</Text>
-          <Text>/</Text>
-          <Text>VOTING</Text>
-          {/* <Text>/</Text>
-          <Text>PAST VICTORS</Text> */}
+        <button type="button" onClick={() => router.push('/landing')}>
+          HOME
+        </button>
+        <Text>/</Text>
+        <button type="button" onClick={() => router.push('/voting')}>
+          VOTING
+        </button>
         </HStack>
       </HStack>
     </Box>
